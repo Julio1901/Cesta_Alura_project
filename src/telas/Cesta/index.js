982274b8eeddc733e4bc8176a225397e6127cbec
Julio Cesar <juliocesar@Julios-MacBook-Pro.local>
2023-05-22 10:40:25 -0300

    Details component created

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Image, StyleSheet, Dimensions, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
-import logo from '../../../assets/logo.png';
-import Texto from '../../componentes/Texto';
-import Topo from './componentes/Topo'
 
+import Detalhes from './componentes/Detalhes';
+import Topo from './componentes/Topo';
 
 
 
@@ -16,15 +15,7 @@ export default function Cesta() {
         <Topo />
 
         <View style={estilos.cesta}>
-            <Texto style={estilos.nome}>Cesta de verduras</Texto>
-            <View style={estilos.fazenda}>
-                <Image source={logo} style={estilos.imagemFazenda}/>
-                <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
-            </View>
-            <Texto style={estilos.descricao}>Uma cesta com produtos selecionados
-             cuidadosamente da fazenda direto para a cozinha
-             </Texto>
-            <Texto style={estilos.preco}>R$ 40,00</Texto>
+           <Detalhes/>
         </View>
    </>
 }
@@ -33,36 +24,6 @@ const estilos = StyleSheet.create({
     cesta: {
         paddingVertical: 8,
         paddingHorizontal: 16
-    },
-    nome: {
-        color: "#464646",
-        fontSize: 26,
-        lineHeight: 42,
-        fontWeight: "bold"
-    },
-    fazenda:{
-        flexDirection: "row",
-        paddingVertical: 12
-    },
-    imagemFazenda: {
-        width: 32,
-        height: 32
-    },
-    nomeFazenda: {
-        fontSize: 16,
-        lineHeight: 26,
-        marginLeft: 12
-    },
-    descricao: {
-        color: "#A3A3A3",
-        fontSize: 16,
-        lineHeight: 26
-    },
-    preco: {
-        color: "#2A9F85",
-        fontWeight: "bold",
-        fontSize: 26,
-        lineHeight: 42,
-        marginTop: 8
     }
+    
 });
